b86f7130098010b24197bc1b970aaded298b9589
Pavan-B-N <pavangowda56005@gmail.com>
2022-12-09 22:23:43 +0530

    origin *

--- server.js
+++ server.js
@@ -16,7 +16,8 @@ app.get("/",(req,res)=>{
 
 const io=new Server(httpServer,{
     cors:{
-        origin:["http://localhost:3000","https://fileshareapp.netlify.app/","http://192.168.0.147:3000"]
+        origin:"*"
+        // origin:["http://localhost:3000","https://fileshareapp.netlify.app/","http://192.168.0.147:3000"]
     }
 })
 
